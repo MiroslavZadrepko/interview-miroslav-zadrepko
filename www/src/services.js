29292c6b89c70beb0bf132ae9bef75b952dea4eb
MiroslavZadrepko <miroslav.zadrepko@gmail.com>
2022-11-14 13:26:57 +0100

    store setup

--- www/src/services.js
+++ www/src/services.js
@@ -4,5 +4,5 @@ const API_URL = '/api/todos'
 
 export const getAllTodos = () => axios.get(`API_URL`);
 export const addTodo = ()=> axios.post(`API_URL`);
-export const editTodo = () => axios.put(`API_URL/${id}`);
+export const editTodo = (id) => axios.put(`API_URL/${id}`);
 export const deletTodo = (id) => axios.delete(`API_URL/${id}`);
