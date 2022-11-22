25bd775602338b83ea125c80afb6df527e88e8ab
MiroslavZadrepko <miroslav.zadrepko@gmail.com>
2022-11-22 13:56:12 +0100

    working without caddy

--- api/server.js
+++ api/server.js
@@ -3,13 +3,16 @@ const dotenv = require('dotenv').config();
 const { errorHandler } = require('./middleware/errorMiddleware.js')
 const conectToDB = require('./conf/db.js')
 const port = process.env.PORT || 5000;
-const host = process.env.HOST 
+const cors =require('cors')
 const app = express();
 
 conectToDB();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
+app.use(cors({
+    origin: 'http://localhost:8080'
+}))
 app.use('/api/todos', require('./routes/todoRoutes'))
 app.use(errorHandler)
 
